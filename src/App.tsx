e37d995ddbb1f1d9ab0e9f9733535ffd19bd96d1
Caiyifan <1657050906@qq.com>
2021-04-08 00:38:36 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App(): JSX.Element {
+    const [data, setData] = useState([0,1,2,3,4,5])
+    useEffect(() => {
+        console.log(data)
+    },[])
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <div onClick={() => {setData([1])}}>删除第一位</div>
+            {data.map((item,index) => {
+                return (
+                    <div key={index}>{item}</div>
+                )
+            })}
         </div>
     );
 }
